b3efd590a3ad3bce531d5908673c8b60cc879e2f
fahid2002 <fahid23105101323.diu.edu.bd>
2026-06-19 22:52:12 +0600

    allprompts

--- src/app/all-prompts/page.js
+++ src/app/all-prompts/page.js
@@ -1,1 +1,11 @@
-import { Suspense } from 'react'; import AllPromptsClient from '@/components/prompts/AllPromptsClient.js'; export default function Page(){return <Suspense><AllPromptsClient/></Suspense>}
+import { Suspense } from 'react';
+import AllPromptsClient from '@/components/prompts/AllPromptsClient.js';
+
+export default function Page() {
+  return (
+    // Suspense is used because AllPromptsClient uses useSearchParams
+    <Suspense>
+      <AllPromptsClient />
+    </Suspense>
+  );
+}
