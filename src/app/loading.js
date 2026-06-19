080dbba63ce7714d3fc7231cd119a46ca7a28236
fahid2002 <fahid23105101323.diu.edu.bd>
2026-06-19 23:02:50 +0600

    loading

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,1 +1,8 @@
-export default function Loading(){return <div className="mx-auto max-w-7xl px-4 py-20"><div className="skeleton h-72 rounded-[2rem]"/></div>}
+export default function Loading() {
+  return (
+    <div className="mx-auto max-w-7xl px-4 py-20">
+      {/* Page loading skeleton */}
+      <div className="skeleton h-72 rounded-[2rem]" />
+    </div>
+  );
+}
