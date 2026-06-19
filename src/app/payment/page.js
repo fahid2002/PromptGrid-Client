5ba861036d7e93b8cedba7d5f05f9b1ff11b1769
fahid2002 <fahid23105101323.diu.edu.bd>
2026-06-19 22:54:42 +0600

    payment

--- src/app/payment/page.js
+++ src/app/payment/page.js
@@ -1,1 +1,11 @@
-import { Suspense } from 'react';import PaymentPage from '@/components/payment/PaymentPage.js';export default function Page(){return <Suspense><PaymentPage/></Suspense>}
+import { Suspense } from 'react';
+import PaymentPage from '@/components/payment/PaymentPage.js';
+
+export default function Page() {
+  return (
+    // Suspense is used because PaymentPage uses useSearchParams
+    <Suspense>
+      <PaymentPage />
+    </Suspense>
+  );
+}
