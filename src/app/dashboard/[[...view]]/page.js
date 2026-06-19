cec6446836036f2541a8dfc67289c4a21b28fd92
fahid2002 <fahid23105101323.diu.edu.bd>
2026-06-19 22:52:48 +0600

    dashboard

--- src/app/dashboard/[[...view]]/page.js
+++ src/app/dashboard/[[...view]]/page.js
@@ -1,1 +1,8 @@
-import DashboardClient from '@/components/dashboard/DashboardClient.js';export default function Page(){return <DashboardClient/>}
+import DashboardClient from '@/components/dashboard/DashboardClient.js';
+
+export default function Page() {
+  return (
+    // DashboardClient handles private route protection internally
+    <DashboardClient />
+  );
+}
